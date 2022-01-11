a56f22352e214c4575df55466d9b3c703f8d2bed
mafik2 <marek.fik@educanet.cz>
2022-01-11 13:48:51 +0100

    100%

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ class Transaction {
 
 let transactions = [
     new Transaction(true, 1000, "Na jidlo"),
-    new Transaction(false, 5000, "za piti"),
+    new Transaction(false, 5000, "za oběd"),
     new Transaction(false, 100, "pro sama"),
-    new Transaction(true, 3000, "dávky"),
+    new Transaction(true, 3000, "výplata"),
 
 ]
 window.onload = () => {
@@ -29,8 +29,7 @@ function writeTransactions() {
             li.innerHTML = "<i class=\"far fa-arrow-alt-circle-up\"></i>"
             li.style.color = "green";
         }
-        li.innerHTML += "<div>" + transactions[i].castka + ",-" + "</div>";
-        li.innerHTML += "<div>" +transactions[i].popis  + "</div>";
+        li.innerHTML += "<a>" + transactions[i].castka + ",-" + "</a>" +"<a>" +transactions[i].popis  + "</a>";
         li.classList = "list-group-item d-flex justify-content-between h4";
         list.appendChild(li);
     }
